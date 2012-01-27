43ab7d75754d51aba05819619553c210e11889c2
chadklassen1@gmail.com <chadklassen1@gmail.com@b6dedcac-f8b6-b7c9-e359-98e7ae5e6d10>
2012-01-27 01:58:16 +0000

    Changed spellOutTypeName function to be more efficiant.

--- token.cpp
+++ token.cpp
@@ -65,72 +65,10 @@ string Token::toString()
 }
 
 string spellOutTypeName(Type name){
-     if (name == IDENTIFIER)
-        return "IDENTIFIER";
-     else if (name == NUMERAL)
-        return "NUMERAL";
-     else if (name == BADNUMERAL)
-        return "BADNUMERAL";
-     else if (name == BADNAME)
-        return "BADNAME";
-     else if (name == BADSYMBOL)
-        return "BADSYMBOL";
-     else if (name == BADCHAR)
-        return "BADCHAR";
-     else if (name == NEWLINE)
-        return "NEWLINE";
-     else if (name == NONAME)
-        return "NONAME";
-     else if (name == ENDOFFILE)
-        return "ENDOFFILE";
-     else if (name == DIV)
-        return "DIV";
-     else if (name == MOD)
-        return "MOD";
-     else if (name == SC)
-        return "SC";
-     else if (name == PLUS)
-        return "PLUS";
-     else if (name == MINUS)
-        return "MINUS";
-     else if (name == TIMES)
-        return "TIMES";
-     else if (name == DIVIDE)
-        return "DIVIDE";
-     else if (name == LB)
-        return "LB";
-     else if (name == RB)
-        return "RB";
-     else if (name == UNKNOWN)
-        return "UNKNOWN";
-     else if (name == PERIOD)
-        return "PERIOD";
-     else if (name == COMMA)
-        return "COMMA";
-     else if (name == LSB)
-        return "LSB";
-     else if (name == RSB)
-        return "RSB";
-     else if (name == AND)
-        return "AND";
-     else if (name == PIPE)
-        return "PIPE";
-     else if (name == TILDA)
-        return "TILDA";
-     else if (name == LT)
-        return "LT";
-     else if (name == GT)
-        return "GT";
-     else if (name == EQUALS)
-        return "EQUALS";
-     else if (name == FORSLASH)
-        return "FORSLASH";
-     else if (name == COLONEQUALS)
-        return "COLONEQUALS";
-     else if (name == BOTHSQUAREBRACKETS)
-        return "BOTHSQUAREBRACKETS";
-     else if (name == ARROW)
-        return "ARROW";
+  
+    int index = name-256;
+    
+    return typeSpelling[index];
 
      }
 
