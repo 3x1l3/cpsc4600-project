8770c0197735536ad986cb1ed59aa635c2f44170
tomcatadam@gmail.com <tomcatadam@gmail.com@b6dedcac-f8b6-b7c9-e359-98e7ae5e6d10>
2012-04-18 16:32:55 +0000

    added a couple of test files, discovered a problem. Our array access does not actually get the array data, but rather, the index is used. i.e. write A[50] writes 50 rather than the value at A[50]. Additionally, I found an error I cannot reproduce, where I try to write an int to the screen and the value (after having tried to write an array) is messed up.

--- trunk/simple4.pl
+++ trunk/simple4.pl
@@ -6,18 +6,18 @@ integer k, l, m, n;
 
 proc Q
 begin
-integer a, i;
-integer j;
-integer k, l, m, n;
-a := 0;
-i := 5;
-j := i;
-m := i * j;
-l := 50;
-write a, i, j, m, l;
+  integer a, i;
+  integer j;
+  integer k, l, m, n;
+  a := 0;
+  i := 5;
+  j := i;
+  m := i * j;
+  l := 50;
+  write a, i, j, m, l;
 end;	
 
-call Q
+call Q;
 
 i := 5;
 j := i;
